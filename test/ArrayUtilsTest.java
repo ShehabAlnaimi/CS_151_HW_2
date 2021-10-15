10406991373e136a1d7eff203cec0224b2e1c4c5
Shehab Alnaimi <shehab.alnaimi@sjsu.edu>
2021-10-15 10:37:14 -0700

    Implemented sum and created unit test for it

--- test/ArrayUtilsTest.java
+++ test/ArrayUtilsTest.java
@@ -1,2 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class ArrayUtilsTest {
+
+    @Test
+    public void testSum(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        int sum = ArrayListUtils.sum(numbers);
+        Assert.assertEquals(15, sum);
+    }
+
 }
