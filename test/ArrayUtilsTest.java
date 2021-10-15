d323e14d8a7df9e7e729ee8abddf4d11373a6a13
Shehab Alnaimi <shehab.alnaimi@sjsu.edu>
2021-10-15 10:46:21 -0700

    Implemented mean method and created unit test for it

--- test/ArrayUtilsTest.java
+++ test/ArrayUtilsTest.java
@@ -1,2 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class ArrayUtilsTest {
+
+    @Test
+    public void testMean(){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        int mean = ArrayListUtils.mean(list);
+        Assert.assertEquals(3, mean);
+        list.remove(0);
+        mean = ArrayListUtils.mean(list);
+        Assert.assertEquals(3, mean);
+    }
+
 }
